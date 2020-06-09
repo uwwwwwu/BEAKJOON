3555b7a2b67546740eaa3409ab294de8df41fa4a
oeh <oehoeh0131@gmail.com>
2020-06-09 09:26:25 +0900

    20200609

---
+++ BAEKJOON_python/A_B_10950.py
@@ -0,0 +1,5 @@
+num=int(input())
+
+for i in range(num):
+    a,b = input().split()
+    print(int(a)+int(b))
---
+++ BAEKJOON_python/A_B_10951.py
@@ -0,0 +1,7 @@
+while 1:
+    try:
+        a,b = input().split()
+        print(int(a)+int(b))
+    except:
+        break
+    
